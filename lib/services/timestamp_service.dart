3b04a3880ad6c07670c63b05c42ebd41af899f79
Azzahra Rinaldi <zaza@Azzahras-Macbook.local>
2025-01-28 08:53:38 +0700

    finishing timestap config

--- lib/services/timestamp_service.dart
+++ lib/services/timestamp_service.dart
@@ -1,5 +1,6 @@
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart'; // ngurusin timestamp
 
+// function untuk data waktu secara real time
 void setDateTime(Function(String, String, String) onDateTimeSet) {
     var  dateNow = DateTime.now();
     var  dateFormat = DateFormat('dd mm yyyy');
@@ -10,4 +11,23 @@ void setDateTime(Function(String, String, String) onDateTimeSet) {
     String currentDate = '$date | $time';
 
     onDateTimeSet(date, time, currentDate);
+}
+
+// untuk status telat atau ngga nya
+void setAttendanceStatus(Function(String) onStatusSet) {
+    var dateNow = DateTime.now();
+    // int.parse ubah int jadi string
+    var hour = int.parse(DateFormat('hh').format(dateNow));
+    var minute = int.parse(DateFormat('mm').format(dateNow));
+
+    String attendanceStatus;
+    if (hour <= 7 || (hour == 7 && minute == 0)) {
+        attendanceStatus = 'Attend';
+    } else if (hour > 7 || (hour == 7 && minute >= 01)) {
+        attendanceStatus = 'Late';
+    } else {
+        attendanceStatus = 'Absent';
+    }
+
+    onStatusSet(attendanceStatus);
 }
