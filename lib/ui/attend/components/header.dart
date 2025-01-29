9d390497d2f443b00540d63514c797b236b2ea21
Azzahra Rinaldi <zaza@Azzahras-Macbook.local>
2025-01-29 10:15:28 +0700

    making header component

---
+++ lib/ui/attend/components/header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Container builderHeader() {
+  return Container(
+    height: 50,
+    decoration: const BoxDecoration(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10)
+      ),
+      color: Colors.blueAccent
+    ),
+    child: const Row(
+      children: [
+        SizedBox(width: 12),
+        Icon(Icons.face_retouching_natural_outlined,
+        color: Colors.white,
+        ),
+        SizedBox(width: 12),
+        Text(
+          "Please take a picture",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
+          ),
+        )
+      ],
+    ),
+  );
+}
