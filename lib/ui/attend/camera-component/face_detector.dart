c0c0ca8e95c46af0081918fde52cd4d9742e8db3
Azzahra Rinaldi <zaza@Azzahras-Macbook.local>
2025-02-03 10:57:45 +0700

    creating face detector

--- lib/ui/attend/camera-component/face_detector.dart
+++ lib/ui/attend/camera-component/face_detector.dart
@@ -1,3 +1,17 @@
+import 'package:attendance_app/utils/google_mlkit.dart';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
 class FaceDetectorComponent {
-  
+  final FaceDetector faceDetector = GoogleMlkit.vision.faceDetector(FaceDetectorOptions(
+    enableContours: true,
+    enableClassification: true,
+    enableTracking: true,
+    enableLandmarks: true
+  ));
+
+  Future<void> detectFaces(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final faces = await faceDetector.processImage(inputImage);
+  }
 }
