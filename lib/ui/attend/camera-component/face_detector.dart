3bc08390bf68109110e5650b2421be79d50c4e48
Azzahra Rinaldi <zaza@Azzahras-Macbook.local>
2025-02-05 09:31:56 +0700

    finishing face_detector function

--- lib/ui/attend/camera-component/face_detector.dart
+++ lib/ui/attend/camera-component/face_detector.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'dart:ui';
 import 'package:attendance_app/utils/google_mlkit.dart';
 import 'package:camera/camera.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
@@ -13,5 +15,39 @@ class FaceDetectorComponent {
   Future<void> detectFaces(XFile image) async {
     final inputImage = InputImage.fromFilePath(image.path);
     final faces = await faceDetector.processImage(inputImage);
+
+    // kalau faces sudah terdeteksi
+    for (Face face in faces) {
+      // Rect = rectangle 2D
+      //bounding dipakai untuk mengambil component dari gambar
+      final Rect boundingBox = face.boundingBox;
+
+      // untuk menghandle posisi wajah secara vertical
+      final double? verticalPosition = face.headEulerAngleY;
+
+      // untuk handle posisi wajah secara horizontal
+      final double? horizontalPosition = face.headEulerAngleZ;
+
+      // perkondisian apabila face landmark sudah aktif, ditandai oleh (mulut/mata/pipi/hidung/telinga)
+      final FaceLandmark? leftEar = face.landmarks[FaceLandmarkType.leftEar];
+      // kalau tengila kiri terdeteksi akan mengembalikan posisi telinga nya
+      if (leftEar != null) {
+        final Point<int> leftEarPosition = leftEar.position;
+        print("Left Ear Position: $leftEarPosition");
+      }
+
+      // perkondisian apabila wajahnya terdeteksi(ditandai dgn bibir tersenyum)
+      if (face.smilingProbability != null) {
+        final double? smileProbability = face.smilingProbability;
+        print("Smile Probability: $smileProbability");
+      }
+
+      // perkondisian apabila fitur tracking wajah sudah aktif
+      if (face.trackingId != null) {
+        final int? trackingId = face.trackingId;
+        print("Tracking ID: $trackingId");
+      }
+
+    }
   }
 }
