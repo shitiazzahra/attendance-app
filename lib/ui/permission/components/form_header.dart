7334e145b282e41a425cf88773d56f17b47818f3
Azzahra Rinaldi <zaza@Azzahras-Macbook.local>
2025-02-11 09:12:42 +0700

    creating form header placeholder

---
+++ lib/ui/permission/components/form_header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+class FormHeader extends StatelessWidget{
+  final String title;
+  final IconData icon;
+  const FormHeader({super.key, required this.title, required this.icon});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10)
+        ),
+        color: Colors.blueAccent
+      ),
+      child: Row(
+        children: [
+          const SizedBox(width: 12),
+          Icon(icon, color: Colors.white),
+          const SizedBox(width: 12),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white 
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
