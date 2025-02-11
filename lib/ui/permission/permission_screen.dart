228f0193174a6de8511fad12ef1094fe9bebd36c
Azzahra Rinaldi <zaza@Azzahras-Macbook.local>
2025-02-11 09:27:14 +0700

    finishing permission feature

--- lib/ui/permission/permission_screen.dart
+++ lib/ui/permission/permission_screen.dart
@@ -1,4 +1,8 @@
-import 'package:attendance_app/services/location_service.dart';
+import 'package:attendance_app/ui/attend/components/app_bar.dart';
+import 'package:attendance_app/ui/permission/components/form_body.dart';
+import 'package:attendance_app/ui/permission/components/form_header.dart';
+import 'package:attendance_app/ui/permission/components/submit_button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class PermissionScreen extends StatefulWidget {
@@ -11,18 +15,39 @@ class PermissionScreen extends StatefulWidget {
 class _PermissionScreenState extends State<PermissionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-
-  void requestPermission() async {
-    final hasPermission = await handleLocationPermission(context);
-
-    // try {
-    //   if () {
-        
-    //   }
-    // } catch (e) {
-      
-    // }
+    Size size = MediaQuery.of(context).size;
+    
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(context),
+      body: SingleChildScrollView(
+        child: Card(
+          color: Colors.white,
+          margin: const EdgeInsets.fromLTRB(10, 10, 10, 30),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          elevation: 5,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FormHeader(
+                title: "Permission Request Form",
+                icon: Icons.assignment
+              ),
+              FormBody(),
+              SubmitButton(
+                size: size,
+                nameController: TextEditingController(),
+                formController: TextEditingController(),
+                toController: TextEditingController(),
+                dropValueCategories: "Dropdown Content",
+                dataCollection: FirebaseFirestore.instance.collection('dataCollectionValue'),
+              )
+            ],
+          )
+        ),
+      )
+    );
   }
 }
